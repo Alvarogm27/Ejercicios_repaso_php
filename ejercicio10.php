0217cbf0571bc95e2c3c8e0e720ba688386da750
Alvarogm27 <alvarogarciamarquez.ccg@gmail.com>
2024-11-03 11:16:05 +0100

    Ejercicio 10 Decimo ejercicio de repaso del examen de php

---
+++ ejercicio10.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejercicio 10</h1>
+    <p>Escribe un script con una función suma que tome dos parámetros y devuelva su suma. Llama a la función con
+    diferentes valores y muestre los resultados en el navegador.</p>
+
+    <?php
+    function suma($num1, $num2){
+        return $num1 + $num2;
+    }
+    ?>
+</body>
+</html>
