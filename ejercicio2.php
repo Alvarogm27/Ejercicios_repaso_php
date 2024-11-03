c3dc62e2c7699fb260a712335aeb996ebe802373
Alvarogm27 <alvarogarciamarquez.ccg@gmail.com>
2024-11-03 11:12:37 +0100

    Ejercicio 2 Segundo ejercicio del examen de php

---
+++ ejercicio2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejercicio 2</h1>
+    <p>Crea un archivo PHP que defina dos variables numéricas, $a y $b y posteriormente, imprima la suma, resta,
+    multiplicación y división de estas variables.</p>
+    <?php
+    $a = 2;
+    $b = 3;
+    echo $a + $b."<br>";
+    echo $b - $a."<br>";
+    echo $a * $b."<br>";
+    echo $b / $a."<br>";
+
+    ?>
+</body>
+</html>
