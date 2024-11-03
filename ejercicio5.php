66d70b87a397e2c082c490bc80a3d02e210070fd
Alvarogm27 <alvarogarciamarquez.ccg@gmail.com>
2024-11-03 11:14:21 +0100

    Ejercicio 5 Quinto ejercicio de repaso del examen de php

---
+++ ejercicio5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Ejercicio 5
+    </h1>
+    <p>
+    Escribe un script que imprima los números del 1 al 10 en el navegador, cada uno en una nueva línea.
+    </p>
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+        echo $i."<br>";
+        }
+    ?>
+</body>
+</html>
