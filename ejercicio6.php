9a4ce296bf5ebafc180c6e3e5f5a0c73bbaa154f
Alvarogm27 <alvarogarciamarquez.ccg@gmail.com>
2024-11-03 11:14:45 +0100

    Ejercicio 6 Sexto ejercicio de repaso del examen de php

---
+++ ejercicio6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejercicio 6</h1>
+    <p>Crea un script que imprima los números del 1 al 10 usando un bucle while.</p>
+    <?php
+    $i = 1;
+    while ($i <= 10){
+    echo $i."<br>";
+    $i++;
+
+    }
+    ?>
+</body>
+</html>
